b13783d9544b7e69703b7967ed12e8f73f1aa0ce
Alex <alex2323official@gmail.com>
2024-09-24 10:04:01 +0200

    JS localStorage save/delete/read works

--- src/App/Data/Storage/TodoRepository.js
+++ src/App/Data/Storage/TodoRepository.js
@@ -1,3 +1,20 @@
 // TodoRepository (Storage / Persistence)
 
 //     Handles data persistence. It saves the todos to a persistent storage (like localStorage, a database, or an API) and loads them when needed. It abstracts the details of how data is saved and retrieved, ensuring the business logic is decoupled from storage mechanisms.
+
+export class TodoRepository {
+  static localStorageSave(arrayIndex, ArrayOfData) {
+    let stringifyArrayOfData = JSON.stringify(ArrayOfData);
+    localStorage.setItem(arrayIndex, stringifyArrayOfData);
+  }
+
+  static getLocalStorageData(arrayIndex) {
+    let arrayOfItems = localStorage.getItem(arrayIndex);
+    arrayOfItems = JSON.parse(arrayOfItems);
+    return arrayOfItems;
+  }
+
+  static localStorageDelete(arrayIndex) {
+    localStorage.removeItem(arrayIndex);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import { TodoItem } from "./App/Data/Logic/TodoProject/TodoItem/TodoItem.js";
 import { TodoView } from "./App/Data/TodoView.js";
 import { AppController } from "./App/AppController.js";
+import { TodoRepository } from "./App/Data/Storage/TodoRepository.js";
 
 // App Here
 TodoItem.createNewTodo(
@@ -26,3 +27,13 @@ AppController.listenForDelete();
 
 // TODO: ADD BTN FOR ADDING IN FORM USER DATA FOR NEW TODOLIS
 // TODO: Add functionality for user to add todo items
+const arry = {
+  name: "tomek",
+  secondName: "domek",
+};
+
+TodoRepository.localStorageSave(0, arry);
+TodoRepository.localStorageDelete(0);
+
+const tet = TodoRepository.getLocalStorageData(0);
+console.log(tet);
